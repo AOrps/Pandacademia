642ce9e7c885a73d79639cb88c5ef356c80fa124
runargs <39810066+runargs@users.noreply.github.com>
2020-10-11 15:35:37 -0400

    Add flask for /res

--- src/main.py
+++ src/main.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        date = request.form.get('date')
-        return redirect(url_for('booking', date=date))
+        nm = request.form.get('nm')
+        feeling = request.form.get('feeling')
+        temp = request.form.get('temp')
+        sick = request.form.get('sick')
+        sore = request.form.get('sore')
+        contact = request.form.get('contact')
+        location = request.form.get('location')
+        return redirect(url_for('results', nm=nm, feeling=feeling, \
+        temp=temp, sick=sick, sore=sore, contact=contact, \
+        location=location))
     return render_template('index.html', title="🩺 Daily Screening")
 
 @app.route("/info")
@@ -14,9 +22,14 @@ def info():
     return render_template("info.html", title="📚 CDC Info")
 
 @app.route("/res")
-def results():
-    num = 1
-    code=num
+def results(nm, feeling, temp, sick, sore, contact, location):
+    ANSWER_SET = {}
+    ANSWER_SET.add(feeling)
+    ANSWER_SET.add(temp)
+    ANSWER_SET.add(sick)
+    ANSWER_SET.add(sore)
+    ANSWER_SET.add(contact)
+    ANSWER_SET.add(location)
     return render_template("results.html", code=num)
 
 if __name__ == "__main__":
