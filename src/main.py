ff6f9ad6534104a26ed97c009150ccd9ae034cf7
runargs <39810066+runargs@users.noreply.github.com>
2020-10-11 15:19:17 -0400

    Added form HTML

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, jsonify, url_for, redirect  #
 from back_end import *
 app = Flask(__name__)
 
-PROJ_NAME = "Pandacademia.tech"
-
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -11,10 +9,15 @@ def index():
         return redirect(url_for('booking', date=date))
     return render_template('index.html', title="🩺 Daily Screening")
 
+@app.route("/info")
+def info():
+    return render_template("info.html", title="📚 CDC Info")
+
 @app.route("/res")
 def results():
-    code=1
-    return render_template("results.html", proj_name=PROJ_NAME, code=num)
+    num = 1
+    code=num
+    return render_template("results.html", code=num)
 
 if __name__ == "__main__":
     app.run(port=8080)
