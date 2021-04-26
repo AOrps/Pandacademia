813a8622f3ef3d7357530ab190b0a0ad8d30526f
AOrps <aeo34@njit.edu>
2021-04-25 21:39:22 -0400

    Split up templates Design

--- test/main.go
+++ test/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
-	"text/template"
+	"strings"
 )
 
 // CONSTANTS
@@ -13,23 +15,101 @@ const (
 	PORT = 9990
 )
 
+// CUSTOM STRUCTS
+type Link struct {
+	Linkname string
+	Href     string
+	Done     bool
+}
+
+type Header struct {
+	Title string
+}
+
+type Page struct {
+	Links []Link
+}
+
 // PAGES
 
-func home(w http.ResponseWriter, r *http.Request) {
-	index := template.Must(template.ParseFiles("templates/base.html"))
-	index.Execute(w, nil)
+// func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		m := valid
 
+// 	}
+// }
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	var allFiles []string
+	files, err := ioutil.ReadDir("./templates")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".html") {
+			allFiles = append(allFiles, "./templates/"+filename)
+		}
+	}
+
+	templates, err := template.ParseFiles(allFiles...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := Page{
+		Links: []Link{
+			{Linkname: "Questions", Href: "/questions", Done: true},
+			{Linkname: "Data Analytics", Href: "/info", Done: true},
+			{Linkname: "Data Visulizations", Href: "/info", Done: true},
+			{Linkname: "Info", Href: "/info", Done: true},
+		},
+	}
+
+	s1 := templates.Lookup("header.html")
+	s1.ExecuteTemplate(w, "header", &Header{Title: "Home"})
+	fmt.Println()
+	s4 := template.Lookup("navbar.html")
+	// TEMPLATE Name must be referred to in  ExecuteTemplate
+	s2 := templates.Lookup("base.html")
+	s2.ExecuteTemplate(w, "content", data)
+	fmt.Println()
+	s3 := templates.Lookup("footer.html")
+	s3.ExecuteTemplate(w, "footer", nil)
+	fmt.Println()
+	// s3.Execute(os.Stdout, nil)
+	// head := template.Must(template.ParseFiles("templates/header.html"))
+	// head.Execute(w, &Header{Title: "Home"})
+	// index := template.Must(template.ParseFiles("templates/base.html"))
+	// index.Execute(w, data)
+	// foot := template.Must(template.ParseFiles("templates/footer.html"))
+	// foot.Execute(w, nil)
+
+}
+
+func questionsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		fmt.Fprint(w, "/ with POST")
+		fmt.Fprint(w, "Questions with POST")
 	}
 }
 
-func results(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "results")
+func dataAnaHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func dataVizHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func infoHandler(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func resultsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "info")
 }
 
 // MAIN FUNCTION
